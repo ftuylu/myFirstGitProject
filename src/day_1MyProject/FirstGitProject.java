27e84945c4802a0a77ece780491653f14f1bc526
ftuylu <ftavus@yahoo.com>
2020-05-13 11:45:49 +0100

    add FirstGitProject.Java,add secondclassPr.Java

---
+++ src/day_1MyProject/FirstGitProject.java
@@ -0,0 +1,10 @@
+package day_1MyProject;
+
+public class FirstGitProject {
+    public static void main(String[] args) {
+        System.out.println("hello my first Project");
+
+    }
+
+
+}
---
+++ src/day_1MyProject/secondClassPr.java
@@ -0,0 +1,7 @@
+package day_1MyProject;
+
+public class secondClassPr {
+    public static void main(String[] args) {
+        System.out.println("second class");
+    }
+}
