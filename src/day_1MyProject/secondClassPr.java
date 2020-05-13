c3cddc66b5453136d856cafcba24a1d3f08693ea
ftuylu <ftavus@yahoo.com>
2020-05-13 12:01:11 +0100

    update secondClassPr.Java

--- src/day_1MyProject/secondClassPr.java
+++ src/day_1MyProject/secondClassPr.java
@@ -3,5 +3,7 @@ package day_1MyProject;
 public class secondClassPr {
     public static void main(String[] args) {
         System.out.println("second class");
+
+        System.out.println("adding some properties");
     }
 }
